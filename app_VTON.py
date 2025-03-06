772f735b0b8f39b731053c273e6b52b49aa1df49
reggiekush <gpstotts@me.com>
2025-03-05 21:59:50 -0600

    Update app_VTON.py

--- app_VTON.py
+++ app_VTON.py
@@ -420,4 +420,4 @@ with image_blocks as demo:
         api_name='tryon'
     )
 
-image_blocks.launch(inbrowser=True, share=args.share, listen=True, server_port=7812)
+image_blocks.launch(inbrowser=True, share=args.share, server_name="0.0.0.0", server_port=7812)
